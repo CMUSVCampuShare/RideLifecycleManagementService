c84d6cda70387964f745a43fd4b6fcda785e1df9
Yinzhu Su <yinzhus@andrew.cmu.edu>
2023-10-20 23:13:53 -0700

    Initialized RideLifecycleManagement Service with Spring Boot.

---
+++ src/main/java/com/campushare/RideLifecycleManagement/RideLifecycleManagementApplication.java
@@ -0,0 +1,13 @@
+package com.campushare.RideLifecycleManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RideLifecycleManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RideLifecycleManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/campushare/RideLifecycleManagement/RideLifecycleManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campushare.RideLifecycleManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RideLifecycleManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
